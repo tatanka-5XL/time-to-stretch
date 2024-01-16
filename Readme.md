934457f519179e9e474596ed0307b9d72899572f
Přibil Petr <pribil@MND.CZ>
2024-01-16 13:38:09 +0100

    old file commited

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Time to stretch!
+
+A simple script that reminds you after certain working period in the office, to stand up and stretch.
+...can happen to be an app?
---
+++ time_to_stretch.py
@@ -0,0 +1,44 @@
+import winsound
+import time
+import datetime
+
+low_freq = 440
+high_freq = 880
+short_duration = 300
+long_duration = 600
+interruption = 0.3
+pause = 3600
+
+def single_beep():
+    winsound.Beep(low_freq, short_duration)
+
+def double_beep():
+    single_beep()
+    time.sleep(interruption)
+    single_beep()
+
+def final_beep():
+    for i in range(3):
+        winsound.Beep(high_freq, long_duration)
+        time.sleep(interruption)
+
+# morning
+print(f'The script started at {datetime.datetime.now()}')
+for i in range(2):
+    time.sleep(pause)
+    single_beep()
+    time.sleep(pause)
+    double_beep()
+double_beep()
+# lunch time
+time.sleep(pause)
+double_beep()
+double_beep()
+# afternoon
+for i in range(2):
+    time.sleep(pause)
+    single_beep()
+    time.sleep(pause)
+    double_beep()
+final_beep()
+print(f'The script finished at {datetime.datetime.now()}')
